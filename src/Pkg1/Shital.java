d3d76cd2d12db414c0f0f750cb8679ec7e64f87b
shital <shitalkurme61@gmail.com>
2021-07-02 18:36:59 +0530

    file changed

--- src/Pkg1/Shital.java
+++ src/Pkg1/Shital.java
@@ -2,7 +2,7 @@ package Pkg1;
 
 public class Shital {
 	  public int balance = 2000;
-	
+	 int i =10;
 public static void main(String[]args) {
 	Shital s= new Shital();
     System.out.println(s.balance);
